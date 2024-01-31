dd5c3a98d0faa06d0deb77575422cfe9f86f6f5a
kbilgen <kbilgen@kbilgens-MacBook-Pro.local>
2024-01-31 13:36:24 +0300

    Yapılan değişikliklerin açıklaması

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
 
 function App() {
+  const [fonKodu, setFonKodu] = useState('');
+  const [apiKey, setApiKey] = useState('');
+  const [chartData, setChartData] = useState({});
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`https://4ohlv1rvj8.execute-api.eu-west-3.amazonaws.com/prod/yatirimcilar?FonKodu=${fonKodu}`, {
+        headers: {
+          'x-api-key': apiKey
+        }
+      });
+      const data = response.data;
+      setChartData({
+        labels: data.map(item => item.Tarih),
+        datasets: [
+          {
+            label: 'Kişi Sayısı',
+            data: data.map(item => parseFloat(item["Kişi Sayısı"].replace(/\./g, '').replace(',', '.'))),
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          },
+          // Diğer veri setleri burada eklenebilir
+        ]
+      });
+    } catch (error) {
+      console.error('Veri çekme hatası', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={fonKodu}
+        onChange={(e) => setFonKodu(e.target.value)}
+        placeholder="Fon Kodu Girin"
+      />
+      <input
+        type="text"
+        value={apiKey}
+        onChange={(e) => setApiKey(e.target.value)}
+        placeholder="API Anahtarı Girin"
+      />
+      <button onClick={fetchData}>Veriyi Çek</button>
+      {chartData.labels && <Line data={chartData} />}
     </div>
   );
 }
